ccd30c0b2877d17fbc5e756087eab3cc5a176d50
RiteshJariwala <rhjariwala@gmail.com>
2025-06-12 16:57:21 +0530

    Updated code scanner plugin to get correct plugin options into callback for iOS devices
    Updated code scanner plugin to get correct plugin options into callback for iOS devices

--- ios/VisionCameraCodeScanner.swift
+++ ios/VisionCameraCodeScanner.swift
@@ -76,16 +76,15 @@ public class VisionCameraCodeScanner: FrameProcessorPlugin {
        
         var barCodeAttributes: [Any] = []
         var barcodeType : [Any] = []
-        if let barcodeTypes = args?["0"] as? Int{
-            barcodeType = [[barcodeTypes]]
-        }
+        barcodeType = [args?["types"]]
+       
         do {
             try Self.createScanner(barcodeType)
             //print("Barcode Scanner: \(String(describing: Self.barcodeScanner))")
             var barcodes: [Barcode] = []
             barcodes.append(contentsOf: try Self.barcodeScanner!.results(in: visionImage))
             
-            if let options = args?[1] as? [String: Any] {
+            if let options = args?["options"] as? [String: Any] {
                 let checkInverted = options["checkInverted"] as? Bool ?? false
                 if (checkInverted) {
                     guard let buffer = CMSampleBufferGetImageBuffer(frame.buffer) else {
